673638e6e3fdbc2711b82793597c3e8db9f2ef80
UmaYalagapati <112148198+UmaYalagapati@users.noreply.github.com>
2024-06-10 21:58:47 -0400

    bug fixes in services folder

--- src/services/storyServices.js
+++ src/services/storyServices.js
@@ -7,10 +7,16 @@ export default {
     getStoryById(id) {
         return apiClient.get("stories/" + id);
     },
-    getStoryByMemberId(id) {
-        return apiClient.get("stories/" + id);
+    getStoriesByMemberId(memberId) {
+        return apiClient.get("stories/member/" + memberId);
     },
     addStory(story) {
         return apiClient.post("stories/", story);
+    },
+    updateStory(story) {
+        return apiClient.put("stories/" + story.storyId, story);
+    },
+    deleteStory(storyId) {
+        return apiClient.delete("stories/" + storyId);
     }
 };
